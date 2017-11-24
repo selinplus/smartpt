057c510a90326e83291545ec34a5aff27222247e
unknown <selinplus@163.com>
2017-11-24 16:33:48 +0800

    update App.vue

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,14 +1,12 @@
 <template>
-  
+  <router-view></router-view>
 </template>
 
 <script>
 export default {
   name: 'app',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+    user_id: 'test1'
   }
 }
 </script>

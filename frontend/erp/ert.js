3ca48ac9491902f46f5089639fe9666b08e8f5e9
unknown <selinplus@163.com>
2017-12-28 16:30:20 +0800

    modify sket

--- frontend/erp/ert.js
+++ frontend/erp/ert.js
@@ -4,9 +4,17 @@ import VueAxios from 'vue-axios';
 import ert from './ert.vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import colors from 'vuetify/es5/util/colors';
 import router from './router';
 import store from './store';
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.purple.base,
+    secondary: colors.grey.darken1,
+    accent: colors.shades.black,
+    error: colors.red.accent3,
+  },
+});
 
 Vue.use(VueAxios, axios);
 new Vue({
--- frontend/erp/ert.vue
+++ frontend/erp/ert.vue
@@ -1,5 +1,5 @@
 <template>
-   <v-app dark>
+   <v-app>
       <v-navigation-drawer app stateless hide-overlay v-model="drawer">
         <v-list dense>
           <v-list-tile @click="viewPage('home')">
@@ -30,6 +30,21 @@
             </v-list-tile-content>
           </v-list-tile>
         </v-list>
+        <v-subheader class="mt-3 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+        <v-list>
+          <v-list-tile v-for="item in items2" :key="item.text" avatar @click="">
+            <v-list-tile-avatar>
+              <img :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`" alt="">
+            </v-list-tile-avatar>
+            <v-list-tile-title v-text="item.text"></v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon color="grey darken-1">settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Manage Subscriptions</v-list-tile-title>
+        </v-list-tile>
       </v-navigation-drawer>
       <v-toolbar app dark fixed color="deep-purple">
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
@@ -55,6 +70,13 @@ export default {
         { title: 'Home', icon: 'dashboard', url:'Home' },
         { title: 'Company', icon: 'question_answer', url:'Company'}
       ],
+      items2: [
+        { picture: 28, text: 'Joseph' },
+        { picture: 38, text: 'Apple' },
+        { picture: 48, text: 'Xbox Ahoy' },
+        { picture: 58, text: 'Nokia' },
+        { picture: 78, text: 'MKBHD' },
+      ]
     }
   },
   methods:{
